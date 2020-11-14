0291dcd7d8fc68788fadb1e7e765d14a95862575
ekarla <47325626+ekarla@users.noreply.github.com>
2020-11-14 11:31:05 -0400

    Add files via upload

---
+++ AllPrime.java
@@ -0,0 +1,19 @@
+class AllPrime {
+    public static void main(String args[]) {
+        int i;
+        int k;
+
+        for(i = 1; i <= 10; i++)
+        {
+            int counter = 0;
+            for(k = 1; k <= i; k++)
+            {
+                if( (i % k) == 0)
+                    ++counter;  
+            }
+            if( counter == 2 )
+                System.out.println("The number: " + i + "is prime");
+        }
+    }
+}
+
